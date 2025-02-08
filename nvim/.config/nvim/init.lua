2de15b33036cf3e75bea81e56c3d73b7eaefd598
monlovesmango <monlovesmango@users.noreply.github.com>
2025-02-08 22:44:54 +0000

    nvim: adjust autocomp arrow fn and relative line numbers

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -18,6 +18,7 @@ vim.opt.shiftwidth = 2
 
 -- Make line numbers default
 vim.opt.number = true
+vim.opt.relativenumber = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
 -- vim.opt.relativenumber = true
