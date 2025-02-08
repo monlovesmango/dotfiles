2afd86e023253a935d4bca96b03de6f222d8664f
monlovesmango <monlovesmango@users.noreply.github.com>
2025-02-08 21:20:14 +0000

    nvim: add surround.nvim plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -157,6 +157,9 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<leader>-', '<C-W>s', { desc = 'split window below', remap = true })
+vim.keymap.set('n', '<leader>|', '<C-W>v', { desc = 'split window right', remap = true })
+vim.keymap.set('n', '<leader>wd', '<C-W>c', { desc = 'delete window', remap = true })
 
 -- keybinds for executing code
 vim.keymap.set('n', '<leader>f%', '<cmd>source %<cr>', { desc = 'source [%]' })
