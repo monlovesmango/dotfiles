81e409bf94e4f801c160791e51884b738afc9565
monlovesmango <monlovesmango@users.noreply.github.com>
2025-04-26 17:44:56 +0000

    nvim: add peristence session manager and update alpha shortcuts

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -1,12 +1,12 @@
 return {
-  "goolord/alpha-nvim",
+  'goolord/alpha-nvim',
   dependencies = {
-    "nvim-tree/nvim-web-devicons",
+    'nvim-tree/nvim-web-devicons',
   },
 
   config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.startify")
+    local alpha = require 'alpha'
+    local dashboard = require 'alpha.themes.startify'
 
     dashboard.section.header.val = {
       [[                                                                       ]],
@@ -24,6 +24,14 @@ return {
       [[                                                                       ]],
       [[                                                                       ]],
       [[                                                                       ]],
+    }
+
+    dashboard.section.top_buttons.val = {
+      dashboard.button('l', '[l]ast session', ':lua require("persistence").load() <CR>'),
+      dashboard.button('r', '[r]ecent', ':lua require("telescope.builtin").oldfiles({ cwd_only = true }) <CR>'),
+      dashboard.button('f', '[f]ind file', ':Telescope find_files <CR>'),
+      dashboard.button('e', '[e]xplorer', ':Neotree filesystem reveal toggle left <CR>'),
+      dashboard.button('n', '[n]ew file', '<cmd>ene <CR>'),
     }
 
     alpha.setup(dashboard.opts)
